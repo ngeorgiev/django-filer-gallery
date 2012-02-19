d513fa44173040708c207b1ed92cd69679a75869
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2012-02-19 18:21:13 -0500

    Adds verbose names and a bit of cleanup

--- filer_gallery/models.py
+++ filer_gallery/models.py
@@ -1,28 +1,41 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
+
 from django.db import models
+from django.utils.translation import ugettext as _
+
 from filer.fields.image import FilerImageField
+
 
 class Gallery(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     category = models.ForeignKey('categories.Category')
     pub_date = models.DateTimeField(default=datetime.now)
-  
+
+    class Meta:
+        verbose_name = _('Gallery')
+        verbose_name_plural = _('Galleries')
+
     def __unicode__(self):
         return self.title
-    
+
+
 class GalleryImage(models.Model):
     gallery = models.ForeignKey(Gallery)
     title = models.CharField(max_length=255)
     category = models.ForeignKey('categories.Category', null=True, blank=True)
     pub_date = models.DateTimeField(default=datetime.now)
     image = FilerImageField()
-    
+
+    class Meta:
+        verbose_name = _('Gallery Image')
+        verbose_name_plural = _('Gallery Images')
+
+    def __unicode__(self):
+        return self.title
+
     def save(self, *args, **kwargs):
         if not self.pk:
             self.pub_date = self.image.date_taken
-        super(GalleryImage, self).save(*args, **kwargs)  
-        
-    def __unicode__(self):
-        return self.title
+        super(GalleryImage, self).save(*args, **kwargs)
